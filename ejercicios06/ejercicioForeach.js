2aa87ce802f7a5dd56c812cdab67024790071826
hterreros <harold.terreros.talledo@gmail.com>
2022-05-04 20:51:31 -0500

    cambios con foreach

---
+++ ejercicios06/ejercicioForeach.js
@@ -0,0 +1,9 @@
+console.log('Ejemplo ForEach');
+let arreglo=[50,80,70,72,86];
+
+arreglo.forEach(
+    function(valor,index){
+        console.log('el valor del elemento es '+valor+' del indice '+index);
+    }
+
+)
