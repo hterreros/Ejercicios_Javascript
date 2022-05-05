c10a526bbdcdacdc245beae64ef467959ef5a981
hterreros <harold.terreros.talledo@gmail.com>
2022-05-04 20:16:54 -0500

    ejercicio 6

---
+++ ejercicios06/ejercicio6.js
@@ -0,0 +1,8 @@
+console.log('Segunda Parte de arreglos');
+let arreglo=[50,80,72,60,96];
+console.log('-------------------------');
+console.log('Antes');
+console.log(arreglo);
+arreglo.splice(2,1);
+console.log('Despues');
+console.log(arreglo);
